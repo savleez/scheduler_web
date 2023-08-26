fb0ac06c7a7340e6ab20be54f0c108c46b83d480
Sergio Velez <svleez@gmail.com>
2023-08-25 23:09:20 -0500

    feat: :sparkles: Added models to Django admin panel

--- src/cron/admin.py
+++ src/cron/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from cron.models import Job, JobSchedule
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "script")
+    list_filter = ("owner",)
+    search_fields = ("name", "owner", "script")
+
+
+@admin.register(JobSchedule)
+class JobScheduleAdmin(admin.ModelAdmin):
+    list_display = (
+        "get_job_name",
+        "get_job_owner",
+        "description",
+        "minute",
+        "hour",
+        "day_of_month",
+        "month",
+        "day_of_week",
+    )
+
+    def get_job_name(self, obj):
+        return obj.job.name
+
+    def get_job_owner(self, obj):
+        return obj.job.owner
+
+    get_job_name.short_description = "Nombre del Job"
+    get_job_owner.short_description = "Dueño del Job"
+
+    search_fields = ("description", "get_job_name", "get_job_owner")
